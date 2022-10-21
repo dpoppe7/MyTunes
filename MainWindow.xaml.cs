58cbfb1a6a4564101fcd91bd4643cbbc1e95170f
dpoppe7 <dpoppe@harding.edu>
2022-10-20 19:17:52 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace myTunes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
